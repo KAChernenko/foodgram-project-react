1325c47c4c7252b445d30cab8dae30b2ef5f3f15
Andrei Chernenko <chernenko.andrey@yandex.ru>
2023-06-12 15:19:17 +0300

    follow_admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 
+from .models import Follow
+
 User = get_user_model()
 
 
@@ -11,4 +13,12 @@ class UserAdmin(admin.ModelAdmin):
         'first_name', 'last_name', 'date_joined',)
     search_fields = ('email', 'username', 'first_name', 'last_name')
     list_filter = ('date_joined', 'email', 'first_name')
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "author")
+    search_fields = ("user", "author")
+    list_filter = ("user", "author")
     empty_value_display = '-пусто-'
