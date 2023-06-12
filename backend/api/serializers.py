b38ce23dbf604d4f75b6fd6a42318c77c9821bd9
Andrei Chernenko <chernenko.andrey@yandex.ru>
2023-06-12 21:54:58 +0300

    depl

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -237,7 +237,7 @@ class FavoriteSerializer(serializers.ModelSerializer):
     def validate(self, obj):
         user = self.context["request"].user
         recipe = obj["recipe"]
-        favorite = user.favourites.filter(recipe=recipe).exists()
+        favorite = user.favorites.filter(recipe=recipe).exists()
 
         if self.context.get("request").method == "POST" and favorite:
             raise serializers.ValidationError(
