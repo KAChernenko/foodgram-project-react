4a2b017ff1be690e9b837915d715e64fe2157f97
Andrei Chernenko <chernenko.andrey@yandex.ru>
2023-06-13 13:18:21 +0300

    ogh

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -126,7 +126,7 @@ class RecipeReadSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Recipe
-        fields = ('id', 'tags', 'author', 'ingredients',
+        fields = ('tags', 'author', 'ingredients',
                   'is_favorited', 'is_in_shopping_cart',
                   'name', 'image', 'text', 'cooking_time')
 
