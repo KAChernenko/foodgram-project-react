f7ddb278263824f95afb97cea95b594beb34e184
Andrei Chernenko <chernenko.andrey@yandex.ru>
2023-06-13 10:01:50 +0300

    com

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -1,12 +1,9 @@
 from rest_framework import permissions
 
 
-class IsAdminAuthorOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
+class IsAdminAuthorOrReadOnly(permissions.BasePermission):
     """Разрешение дминистратору/автору"""
 
     def has_object_permission(self, request, obj):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or (request.user == obj.author)
-            or request.user.is_staff
-        )
+        return (request.method in permissions.SAFE_METHODS
+                or obj.author == request.user)
