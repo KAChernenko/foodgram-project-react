752c31a35f9cafb50b22fc56427a4fc76beb80a5
Andrei Chernenko <chernenko.andrey@yandex.ru>
2023-06-15 00:47:05 +0300

    perm1

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -5,6 +5,5 @@ class IsAdminAuthorOrReadOnly(permissions.BasePermission):
     """Разрешение для администратора/автора"""
 
     def has_object_permission(self, request, view, obj):
-        def has_object_permission(self, request, view, obj):
-            return (request.method in permissions.SAFE_METHODS
-                    or obj.author == request.user)
+        return (request.method in permissions.SAFE_METHODS
+                or obj.author == request.user)
